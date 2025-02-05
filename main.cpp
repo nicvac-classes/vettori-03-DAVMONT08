43985df151d5cc2d47ef39f8ab91338fda6125c8
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-02-05 21:57:47 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,37 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+               float n,i;
+               cout<<" inserisci quanti studenti ci sono in classe?"<<endl;
+               cin>> n;
+               vector<string<nomi[n];
+               vector<float< voti[n];
+               float v;
+               int max;
+               i=0;
+               while(i<n)
+            {
+                cout<<"inserisci nome"<<endl;
+                cin>>nomi[i];
+                cout<<"inserisci voto"<<endl;
+                cin>> voti[i];
+                i=i+1;
+            }
+             
+             max=voti[0];
+             i=0;
+             while(i<n)
+             {
+                cout<<"lo studente" << nomi[i] << "voto" << voti[i];
+                if(voti[i]>max)
+                i=i+1;
+             }
+
+            cout<<"il voto max è" <<max <<endl;
+
+
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
